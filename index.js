43b3cde9e3754d70420aa4c5ea89486991af5591
Kevin Bungeneers <kevin.bungeneers@gmail.com>
2016-12-31 13:08:40 +0100

    Exposed HomeControlClient, Action and Location

---
+++ index.js
@@ -0,0 +1,9 @@
+const HomeControlClient = require('./lib/HomeControlClient').HomeControlClient;
+const Action = require('./lib/Action').Action;
+const Location = require('./lib/Location').Location;
+
+module.exports = {
+  HomeControlClient: HomeControlClient,
+  Action: Action,
+  Location: Location
+}
