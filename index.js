f35fb4ef49baa507c57f1544b2450d1f4383bf38
Kevin Bungeneers <kevin.bungeneers@gmail.com>
2017-01-03 22:28:54 +0100

    Automatically connect to NHC installation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const HomeControlClient = require('./lib/HomeControlClient');
 const Action = require('./lib/Action');
 const Location = require('./lib/Location');
+const dgram = require('dgram');
 
 module.exports = {
   HomeControlClient: HomeControlClient,
